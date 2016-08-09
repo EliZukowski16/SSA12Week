570e7f608985d5d0c3efa5efc2b2e8ae74548fcb
Eli Zukowski <eli.zukowski@gmail.com>
2016-08-09 10:42:37 -0400

    Outputting hashcodes in test class

--- src/test/java/com/tiy/ssa/weektwo/assignmenttwo/FooAndGooTest.java
+++ src/test/java/com/tiy/ssa/weektwo/assignmenttwo/FooAndGooTest.java
@@ -19,8 +19,8 @@ public class FooAndGooTest
         Foo fooOne = new Foo("Test");
         Foo fooTwo = new Foo("Test");
         
-        System.out.println(fooOne);
-        System.out.println(fooTwo);
+        System.out.println(fooOne.hashCode());
+        System.out.println(fooTwo.hashCode());
         
         assertTrue(foo.add(fooOne));
         assertTrue(foo.add(fooTwo));
@@ -33,8 +33,8 @@ public class FooAndGooTest
         Goo gooOne = new Goo("Test");
         Goo gooTwo = new Goo("Test");
         
-        System.out.println(gooOne);
-        System.out.println(gooTwo);
+        System.out.println(gooOne.hashCode());
+        System.out.println(gooTwo.hashCode());
         
         assertTrue(goo.add(gooOne));
         assertFalse(goo.add(gooTwo));
