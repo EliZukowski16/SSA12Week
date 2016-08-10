a8093438dd23abab11527d363242fe95041b86f5
Eli Zukowski <eli.zukowski@gmail.com>
2016-08-10 15:45:07 -0400

    Inefficient working implementation of WordCount

--- src/main/java/com/tiy/ssa/weektwo/assignmentthree/WordCount.java
+++ src/main/java/com/tiy/ssa/weektwo/assignmentthree/WordCount.java
@@ -1,19 +1,20 @@
 package com.tiy.ssa.weektwo.assignmentthree;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class WordCount
 {
-    
-    List<String> allWords = new ArrayList<>();
+    private List<String> allWords = new ArrayList<>();
 
-    /**
-     * 
-     * @param word
-     *            callers are responsible for ensuring <code>word</code> <br>
-     *            is an actual word, parsing is done by the caller
-     */
+    private enum Direction
+    {
+        BOTTOM, TOP
+    };
+
     public void addWord(String word)
     {
         allWords.add(word.toLowerCase());
@@ -22,140 +23,118 @@ public class WordCount
     public int count(String word)
     {
         int occurrenceOfWord = 0;
-        for(String s : allWords)
+        for (String s : allWords)
         {
-            if(s.equals(word.toLowerCase()))
+            if (s.equals(word.toLowerCase()))
             {
                 occurrenceOfWord++;
             }
         }
-        
+
         return occurrenceOfWord;
 
     }
 
-    /**
-     *
-     * @param many
-     * @return a logical set of words ordered in descending poularity order, so
-     *         top <code>many</code> words
-     */
     public List<String> top(int many)
     {
-        List<String> topWords = new ArrayList<>();
-        List<Integer> occurrenceOfWords = new ArrayList<>();
-        
-        List<String> wordsToReturn = new ArrayList<>();
-       
-        for(String s : allWords)
-        {
-            if(!(topWords.contains(s)))
-            {
-                topWords.add(s);
-                occurrenceOfWords.add(this.count(s));
-            }
-        }
-        
-        for(int i = 0; i < many; i++)
-        {
-            int occurrenceOfWord = 0;
-            int indexOfWord = 0;
-            
-            for(int j = 0; j < topWords.size(); j++)
-            {
-                if(occurrenceOfWords.get(j) >= occurrenceOfWord)
-                {
-                    occurrenceOfWord = occurrenceOfWords.get(j);
-                    indexOfWord = j;
-                }
-            }
-            
-            wordsToReturn.add(topWords.get(indexOfWord));
-            occurrenceOfWords.remove(indexOfWord);
-            topWords.remove(indexOfWord);
-        }
-        
-        return wordsToReturn;
-        
-
+        return this.getWordSubset(this.getWordCounts(), Direction.TOP, many);
     }
 
-    /**
-     *
-     * @param many
-     * @return a logical set of words ordered in ascending least popularity
-     *         order, so bottom <code>many</code> words
-     */
     public List<String> bottom(int many)
     {
-        List<String> bottomWords = new ArrayList<>();
-        List<Integer> occurrenceOfWords = new ArrayList<>();
-        
+        return this.getWordSubset(this.getWordCounts(), Direction.BOTTOM, many);
+    }
+
+    private List<String> getWordSubset(Map<String, Integer> wordOccurrences, Direction direction, int many)
+    {
         List<String> wordsToReturn = new ArrayList<>();
-       
-        for(String s : allWords)
+
+        for (int i = 0; i < many; i++)
         {
-            if(!(bottomWords.contains(s)))
+            int wordOccurrence = direction == Direction.BOTTOM ? getMaxWordOccurrence() : getMinWordOccurrence();
+            String word = "";
+
+            for (Entry e : wordOccurrences.entrySet())
             {
-                bottomWords.add(s);
-                occurrenceOfWords.add(this.count(s));
-            }
-        }
-        
-        for(int i = 0; i < many; i++)
-        {
-            int occurrenceOfWord = this.getMaxWordOccurrence();
-            int indexOfWord = 0;
-            
-            for(int j = 0; j < bottomWords.size(); j++)
-            {
-                if(occurrenceOfWords.get(j) <= occurrenceOfWord)
+                switch (direction)
                 {
-                    occurrenceOfWord = occurrenceOfWords.get(j);
-                    indexOfWord = j;
+                case BOTTOM:
+                    if (((int) e.getValue() <= wordOccurrence))
+                    {
+                        wordOccurrence = (int) e.getValue();
+                        word = String.valueOf(e.getKey());
+                    }
+                    break;
+                case TOP:
+                    if (((int) e.getValue() >= wordOccurrence))
+                    {
+                        wordOccurrence = (int) e.getValue();
+                        word = String.valueOf(e.getKey());
+                    }
+                    break;
                 }
             }
-            
-            wordsToReturn.add(bottomWords.get(indexOfWord));
-            occurrenceOfWords.remove(indexOfWord);
-            bottomWords.remove(indexOfWord);
+
+            wordsToReturn.add(word);
+            wordOccurrences.remove(word);
         }
-        
+
         return wordsToReturn;
 
     }
-    
+
+    private Map<String, Integer> getWordCounts()
+    {
+        Map<String, Integer> wordOccurrences = new HashMap<>();
+
+        for (String s : allWords)
+        {
+            wordOccurrences.putIfAbsent(s, count(s));
+        }
+
+        return wordOccurrences;
+    }
+
     private int getMaxWordOccurrence()
     {
         int maxWordOccurrence = 0;
-        
-        for(String s : allWords)
+
+        for (String s : allWords)
         {
-            if(this.count(s) >= maxWordOccurrence)
+            if (this.count(s) >= maxWordOccurrence)
             {
                 maxWordOccurrence = count(s);
             }
         }
-        
+
         return maxWordOccurrence;
     }
 
-    /**
-     *
-     * @return the input that is being word counted (maintaining the order
-     *         prescribed by {@link #addWord(String) }
-     * @see #addWord(String)
-     */
+    private int getMinWordOccurrence()
+    {
+        int minWordOccurrence = this.getMaxWordOccurrence();
+
+        for (String s : allWords)
+        {
+            if (this.count(s) <= minWordOccurrence)
+            {
+                minWordOccurrence = count(s);
+            }
+        }
+
+        return minWordOccurrence;
+    }
+
     public String source()
     {
         String source = "";
-        
-        for(String s : allWords)
+
+        for (String s : allWords)
         {
-            source += s;
+            source += s + " ";
         }
-        
-        return source;
+
+        return source.trim();
 
     }
 }
